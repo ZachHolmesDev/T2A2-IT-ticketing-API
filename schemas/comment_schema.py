4fd233c79ecaf2b74f9ee0a44daad62bc62e4f09
zach <zholmes430@gmail.com>
2023-07-29 16:46:10 +1000

    rename to bluprint

--- schemas/comment_schema.py
+++ schemas/comment_schema.py
@@ -1,10 +1,10 @@
-from main import ma
-from marshmallow import fields
+from main import db 
 
 
-class CommentSchema(ma.Schema):
-    class Meta:
-        fields = ('id', 'content', 'created_at', 'user_id', 'ticket_id')
-
-comment_schema = CommentSchema()
-comments_schema = CommentSchema(many=True)
+class Comment(db.Model):
+    __tablename__ = 'comment'
+    id         = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    content    = db.Column(db.Text)
+    created_at = db.Column(db.DateTime)
+    user_id    = db.Column(db.Integer, db.ForeignKey('users.id'))
+    ticket_id  = db.Column(db.Integer, db.ForeignKey('tickets.id'))
