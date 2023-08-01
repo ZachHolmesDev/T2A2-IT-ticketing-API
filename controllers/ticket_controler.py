43055e92ec3cb25c936757a3d19f77e0d74df779
zach <zholmes430@gmail.com>
2023-08-01 20:49:35 +1000

    cleanup

--- controllers/ticket_controler.py
+++ controllers/ticket_controler.py
@@ -1,6 +1,6 @@
 from datetime import timedelta
 from flask import Blueprint, jsonify, abort, request
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import jwt_required
 from marshmallow.exceptions import ValidationError
 from main import db, bcrypt
 
@@ -14,32 +14,24 @@ tickets_bp = Blueprint("tickets", __name__, url_prefix="/tickets")
 
 # GET /tickets: Retrieves a list of all tickets
 @tickets_bp.get("/")
+@jwt_required()
 def get_all_tickets():
     tickets_query = db.select(Ticket)
     tickets       = db.session.scalars(tickets_query)
     return tickets_schema.dump(tickets)
     
     
-    
-
-
 # GET /tickets/<id>: Retrieves a specific ticket by its ID
 @tickets_bp.get('/<int:id>')
+@jwt_required()
 def get_ticket_by_id(id): 
     stmt   = db.select(Ticket).filter_by(id=id)
     ticket = db.session.scalar(stmt)
     return ticket_schema.dump(ticket)
 
 
-@tickets_bp.get('/<int:id>/comments')
-def get_ticket_and_comments_by_id(id): 
-    stmt   = db.select(Ticket ).filter_by(id=id)
-    ticket = db.session.scalars(stmt)
-    return tickets_schema.dump(ticket)
-
-
 # POST /tickets: Creates a new ticket
 
-# PUT /tickets/<id>: Updates a specific ticket by its ID
+# PUT/PATCH /tickets/<id>: Updates a specific ticket by its ID
 
 # DELETE /tickets/<id>: Deletes a specific ticket by its ID
