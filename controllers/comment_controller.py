74c2d278ea0347eeccf9da7ee396c0a3d39da5fe
zach <zholmes430@gmail.com>
2023-08-04 18:55:26 +1000

    added comment validation

--- controllers/comment_controller.py
+++ controllers/comment_controller.py
@@ -39,22 +39,30 @@ def get_comment_by_id(id):
 @jwt_required()
 def create_comment():
     try:
-        comment_data = request.get_json()
-        new_comment  = Comment(
-                              ticket_id  = comment_data.get('ticket_id'),
-                              content    = comment_data.get('content'),
-                              created_at = datetime.now(),
-                              user_id    = get_jwt_identity() 
-                              )
+        # Get the JSON data from the request
+        comment_request = request.get_json()
+        # Validate the data using the comment schema
+        comment_data = comment_schema.load(comment_request, partial=True)
+
+        # Create a new comment instance using the validated data
+        new_comment = Comment(
+            ticket_id  = comment_data.get('ticket_id'),
+            content    = comment_data.get('content'),
+            created_at = datetime.now(),
+            user_id    = get_jwt_identity()
+        )
+        # Add and commit the new comment to the database
         db.session.add(new_comment)
         db.session.commit()
 
         return comment_schema.dump(new_comment), 201
     except ValidationError as err:
+        # Validation error, return 400 response
         return {"message": "Validation Error", "errors": err.messages}, 400
-    # might need to make more descriptive but not sure about just saying no ticket incase of different integ err ? 
+    # Might need to make more descriptive, but not sure about just saying no ticket in case of different integrity error
     except IntegrityError as err:
-        return {"message": "IntegrityError", "errors": f'{err.orig}'}, 400
+        return {"message": "IntegrityError, you may be trying to post a comment without a ticket ID", "errors": f'{err.orig}'}, 400
+
 
 # PUT/PATCH /comments/<id>: Updates a specific comment by its ID
 @comments_bp.put('/<int:id>')
@@ -63,31 +71,34 @@ def create_comment():
 @check_permissions_wrap
 def update_comment(id, user_role):
     try:
-        # Load comment schema with new comment info from the request
-        comment_data = comment_schema.load(request.get_json())
-        # find chosen comment
-        stmt         = db.select(Comment).filter_by(id=id)
-        comment      = db.session.scalar(stmt)
+        # Get the JSON data from the request
+        comment_request = request.get_json()
+        # Validate the data using the comment schema
+        comment_data = comment_schema.load(comment_request, partial=True)
+
+        # Find chosen comment
+        stmt    = db.select(Comment).filter_by(id=id)
+        comment = db.session.scalar(stmt)
         if not comment:
             return {'error': f'Comment not found with id {id}'}, 404
 
-        if comment:
-            # check permissions
-            if str(comment.user_id) != get_jwt_identity() and user_role.can_edit_all == False:
-                return {'error': 'Unauthorized'}, 403
-            # if permission change the comment to link to a different ticket 
-            if user_role.can_edit_all == True:
-                comment.ticket_id  = comment_data.get('ticket_id')  or comment.ticket_id
-            else:
-                comment.ticket_id
-            comment.content    = comment_data.get('content') or comment.content
-            
-            db.session.commit()
-            return comment_schema.dump(comment)
+        # Check permissions
+        if str(comment.user_id) != get_jwt_identity() and user_role.can_edit_all == False:
+            return {'error': 'Unauthorized'}, 403
+        
+        # If permission, user can change the comment to link to a different ticket 
+        if user_role.can_edit_all == True:
+            comment.ticket_id = comment_data.get('ticket_id') or comment.ticket_id
+        # change the content
+        comment.content = comment_data.get('content') or comment.content
+        
+        db.session.commit()
+        return comment_schema.dump(comment)
+    except ValidationError as err:
+        # Validation error, return 400 response
+        return {"message": "Validation Error", "errors": err.messages}, 400
     except IntegrityError as err:
         return {"message": "IntegrityError", "errors": f'{err.orig}'}, 400
-        
-        
 
 
 # DELETE /comments/<id>: Deletes a specific comment by its ID
